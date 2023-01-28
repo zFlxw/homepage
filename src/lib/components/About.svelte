b390c962b9a0bdb2a7e226af13bf194eb5ecc21f
Maik <58326890+zFlxw@users.noreply.github.com>
2023-01-28 13:13:11 +0100

    style: sizing of hero (#2)

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,5 @@
+<div class="h-screen bg-arctic mt-24 w-full text-center">
+    <div class="relative inline-block">
+        <h1 class="text-8xl font-semibold mt-12 text-center block cool-hr">About Me</h1>
+    </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-    import Navbar from '$lib/components/Navbar.svelte';
-    import Hero from '$lib/components/Hero.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
+	import Hero from '$lib/components/Hero.svelte';
+	import About from '$lib/components/About.svelte';
 </script>
 
 <Navbar />
 <Hero />
+<About />
